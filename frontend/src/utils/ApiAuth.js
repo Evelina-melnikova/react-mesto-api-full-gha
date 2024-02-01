731498fd7fbd257523517413b658e7487e05ce7e
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-01 05:37:15 +0300

    фикс ошибок при входе и регистрации

--- frontend/src/utils/ApiAuth.js
+++ frontend/src/utils/ApiAuth.js
@@ -1,17 +1,27 @@
 export const BASE_URL = 'https://api.mesto.evelina.nomoredomainsmonster.ru';
 
-function getReq(res) {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Ошибка ${res.status}`);
+function getReq(url, options) {
+  return fetch(url, options)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((errorData) => {
+          const errMessage = errorData.message || 'Request failed';
+          const errStatus = new Error(errMessage);
+          errStatus.status = res.status;
+          throw errStatus;
+        });
+      }
+    })
 }
 
 
 export const register = (password, email) => {
-  return fetch(`${BASE_URL}/signup`, {
+  return getReq(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
+      'Accept': 'application/json',
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
@@ -23,21 +33,22 @@ export const register = (password, email) => {
 }
 
 export const authorize = (password, email) => {
-  return fetch(`${BASE_URL}/signin`, {
+  return getReq(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
+      'Accept': 'application/json',
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ 
+    body: JSON.stringify({
       email: email,
       password: password
-     })
+    })
   })
     .then(getReq)
 }
 
 export const getContent = (token) => {
-  return fetch(`${BASE_URL}/users/me`, {
+  return getReq(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       "Content-Type": "application/json",

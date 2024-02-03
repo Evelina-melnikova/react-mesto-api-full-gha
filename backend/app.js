ad0d537b6bd00c28d6000da089b7161eae099ee8
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-04 00:44:23 +0300

    пр 15  фикс ошибок

--- backend/app.js
+++ backend/app.js
@@ -11,26 +11,28 @@ const { reqLogger, errLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(json());
+
 app.use(reqLogger);
 app.use(cors);
 app.use(router);
 app.use(errLogger);
-
-app.use(json());
-app.use(errors());
-
-app.use('*', () => {
-  throw new NotFoundError('Такой страницы не существует');
-});
-app.use(error);
-
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+
+app.use(errors());
+app.use(error);
+
+app.use('*', () => {
+  throw new NotFoundError('Такой страницы не существует');
+});
+
 app.listen(PORT, () => {
   console.log(`Запущен порт: ${PORT}`);
 });

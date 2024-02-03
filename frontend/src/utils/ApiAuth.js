1c4493b4942b3aebb9a061453794647d7d588c6a
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-03 23:50:56 +0300

    пр 15  фикс ошибок

--- frontend/src/utils/ApiAuth.js
+++ frontend/src/utils/ApiAuth.js
@@ -38,13 +38,13 @@ export const authorize = (password, email) => {
   })
 };
 
-export const getContent = (token) => {
+export const getContent = () => {
   return getReq(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      "Authorization": `Bearer ${token}`
+      // "Authorization": `Bearer ${token}`
     }
   })
 };

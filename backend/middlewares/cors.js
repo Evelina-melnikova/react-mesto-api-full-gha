576f58cfb45b173bdcd4a218588e5a76fbfc0d85
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-04 00:18:03 +0300

    пр 15  фикс ошибок

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,6 +2,7 @@ const allowedCors = [
   'https://mesto.evelina.nomoredomainsmonster.ru',
   'http://mesto.evelina.nomoredomainsmonster.ru',
   'http://localhost:3000',
+
 ];
 
 module.exports = (req, res, next) => {
@@ -10,14 +11,16 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-  const { method } = req;
 
+  const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
+
   return next();
 };

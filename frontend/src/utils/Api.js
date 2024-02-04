5da21c4ef009d3e8854d650cbdff666943ea0e72
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-02-04 12:05:37 +0300

    пр 15  фикс ошибок

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -6,14 +6,11 @@ class Api {
         this._headers = headers;
     }
 
-    _getRequest(options,url) {
-        return fetch(options, url)
-            .then((res) => {
-                if (!res.ok) {
-                    return Promise.reject(`Ошибка: ${res.status}`);
-                }
-                return res.json();
-            });
+    _getRequest(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка ${res.status}`);
     }
 
     getAllCards() {
